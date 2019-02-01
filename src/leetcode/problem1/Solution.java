841947b95c6d35d58c2ab3252890b6badf796fda
caozh <849138487@qq.com>
2019-02-01 12:00:48 +0800

    problem1 AC

---
+++ src/leetcode/problem1/Solution.java
@@ -0,0 +1,17 @@
+package leetcode.problem1;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int a[] = {0,0};
+        for(int i=0;i<nums.length;i++){
+            for(int j = i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    a[0] = i;
+                    a[1] = j;
+                    return a;
+                }
+            }
+        }
+        return a;
+    }
+}
