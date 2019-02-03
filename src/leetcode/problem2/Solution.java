34c74b476b75588348d1dc8fe1ccb394d2c0fb5a
caozheng <849138487@qq.com>
2019-02-03 20:05:18 +0800

    homechange

--- src/leetcode/problem2/Solution.java
+++ src/leetcode/problem2/Solution.java
@@ -14,8 +14,8 @@ class Solution {
         int carry = 0;
         int sum = 0;
         while(l1!=null||l2!=null){
-            int x = (l1!=null) ? l1.val : 0;
-            int y = (l2!=null) ? l2.val : 0;
+            int x = (l1!=null) ? l1.val : 1;
+            int y = (l2!=null) ? l2.val : 1;
             sum = x+y+carry;
             if(sum>=10){
                 carry = 1;
