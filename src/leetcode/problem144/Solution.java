b44ce1782f96148e013d4ec4a57d60a97d1b7076
caozheng <849138487@qq.com>
2019-02-08 14:12:27 +0800

    problem144 AC

---
+++ src/leetcode/problem144/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.problem144;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList();
+        if (root == null) return list;
+        list.add(root.val);
+        list.addAll(preorderTraversal(root.left));
+        list.addAll(preorderTraversal(root.right));
+        return list;
+    }
+}
