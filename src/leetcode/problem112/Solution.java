215ccb3a8333b83b78334dd0cdfa895be5480577
caozh <849138487@qq.com>
2019-02-13 15:44:45 +0800

    problem112 AC 路径ç»总和

---
+++ src/leetcode/problem112/Solution.java
@@ -0,0 +1,34 @@
+package leetcode.problem112;
+
+import java.util.Stack;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        if(root==null) return false;
+        stack.push(root);
+        if(root.val==sum&&root.right==null&&root.left==null) return true;
+        while(!stack.isEmpty()){
+            TreeNode treeNode = stack.pop();
+            if(treeNode.right!=null){
+                treeNode.right.val +=treeNode.val;
+                if(treeNode.right.val==sum&&treeNode.right.left==null&&treeNode.right.right==null) return true;
+                stack.push(treeNode.right);
+            }
+            if(treeNode.left!=null){
+                treeNode.left.val +=treeNode.val;
+                if(treeNode.left.val==sum&&treeNode.left.left==null&&treeNode.left.right==null) return true;
+                stack.push(treeNode.left);
+            }
+        }
+        return false;
+    }
+}
