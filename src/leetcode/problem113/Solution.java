c5d5e02ba343c56d52e2c14256b4aa1320cfa5ba
caozh <849138487@qq.com>
2019-02-14 10:39:41 +0800

    problem113 AC 路径总和Ⅱ

--- src/leetcode/problem113/Solution.java
+++ src/leetcode/problem113/Solution.java
@@ -14,7 +14,7 @@ class TreeNode {
 class Solution {
     private static List<List<Integer>> outerList = new ArrayList<>();
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
-        outerList.clear();  //如果不加这一句outerList中会保存上一次测试示例的数据
+        outerList.clear();  //如果不加这一句outerList中会保存上一次测试示例的数据 因为static变量只加载一次 如果想不用clear 可以向下面注释代码那样写
         List<Integer> innerList = new ArrayList<>();
         if(root==null) {
             return outerList;
@@ -38,3 +38,30 @@ class Solution {
         innerList.remove(innerList.size()-1);
     }
 }
+//class Solution {
+//    // private static List<List<Integer>> outerList = new ArrayList<>();
+//    private List<List<Integer>> outerList = new ArrayList<>();
+//    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+//        //outerList.clear();  //如果不加这一句outerList中会保存上一次测试示例的数据
+//        List<Integer> innerList = new ArrayList<>();
+//        if(root==null) {
+//            return outerList;
+//        }
+//        dfs(root,0,innerList,sum);
+//        return outerList;
+//    }
+//
+//    public void dfs(TreeNode root,int pathSum,List<Integer> innerList,int sum){
+//        innerList.add(root.val);
+//        pathSum+=root.val;
+//        if(root.left==null&&root.right==null&&pathSum==sum){
+//            outerList.add(new ArrayList<>(innerList));    //这里换成outerList.add(innerList)会报错
+//        }
+//        if(root.left!=null){
+//            dfs(root.left,pathSum,innerList,sum);
+//        }
+//        if(root.right!=null){
+//            dfs(root.right,pathSum,innerList,sum);
+//        }
+//        innerList.remove(innerList.size()-1);
+//    }
