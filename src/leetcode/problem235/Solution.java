80d6ae22ec9995835adc6d0c7dfa0f91a23a1de8
caozheng <849138487@qq.com>
2019-02-14 22:47:39 +0800

    problem235 AC 二叉搜索树的最近公共祖先(遇到了(遇到了参数值传递问题)

--- src/leetcode/problem235/Solution.java
+++ src/leetcode/problem235/Solution.java
@@ -1,14 +1,12 @@
 package leetcode.problem235;
 
 
-
 class TreeNode {
       int val;
       TreeNode left;
       TreeNode right;
       TreeNode(int x) { val = x; }
   }
-
 class Solution {
     private TreeNode ancestor = new TreeNode(0);
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
