5773baa6b35c5f2e4f64dd2394592cfd83d15d14
caozheng <849138487@qq.com>
2019-02-15 23:38:01 +0800

    problem257 ING

---
+++ src/leetcode/problem257/Solution.java
@@ -0,0 +1,40 @@
+package leetcode.problem257;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+     int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    public List<List<Integer>> outerList = new ArrayList<>();
+    public List<Integer> innerList = new ArrayList<>();
+    public List<String> binaryTreePaths(TreeNode root) {
+
+
+        dfs(root,innerList);
+        System.out.println(outerList.toString());
+        return null;
+    }
+    public void dfs(TreeNode node,List<Integer> innerList){
+
+        if(node==null) return;
+        innerList.add(node.val);
+        if(node.left!=null){
+            dfs(node.left,innerList);
+        }
+        if(node.right!=null){
+            dfs(node.right,innerList);
+        }
+        outerList.add(innerList);
+        innerList.remove(innerList.size()-1);
+
+
+
+    }
+}
