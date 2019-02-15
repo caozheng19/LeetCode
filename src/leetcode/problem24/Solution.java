8e3951724532958b8215b22d06e2724ed2c3282b
caozh <849138487@qq.com>
2019-02-15 10:25:36 +0800

    problem24 AC 两辆交换链表中的节点

---
+++ src/leetcode/problem24/Solution.java
@@ -0,0 +1,40 @@
+package leetcode.problem24;
+
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head==null) return head;
+        ListNode preNode = head;
+        ListNode curNode = head.next;
+        ListNode tempNode = null;
+        int flag = 0;
+        while(preNode!=null&&curNode!=null){
+
+            preNode.next = curNode.next;
+            curNode.next = preNode;
+            if(flag==1)
+            {
+                tempNode.next = curNode;
+                tempNode = preNode;
+            }
+            if(flag==0){
+                head = curNode;
+                tempNode = preNode;
+                flag = 1;
+            }
+
+            preNode = preNode.next;
+            if(preNode!=null)
+                curNode = preNode.next;
+
+
+        }
+        return head;
+    }
+}
