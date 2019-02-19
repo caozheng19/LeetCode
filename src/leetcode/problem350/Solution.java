0c0f3dd1a61a2e5b65e4c1726c7e26b326ac11df
caozh <849138487@qq.com>
2019-02-19 10:19:32 +0800

    problem350 AC 两个数组的交集Ⅱ

---
+++ src/leetcode/problem350/Solution.java
@@ -0,0 +1,37 @@
+package leetcode.problem350;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer,Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        for(int num:nums1){
+            if(!map.containsKey(num)){
+                map.put(num,1);
+            }else{
+                map.put(num,map.get(num)+1);
+            }
+        }
+        for(int num:nums2){
+            if(map.containsKey(num)){
+                System.out.println(num);
+                if(map.get(num)>1){
+                    map.put(num,map.get(num)-1);
+                    list.add(num);
+                }else{
+                    map.remove(num);
+                    list.add(num);
+                }
+            }
+        }
+        int[] res = new int[list.size()];
+        for(int i = 0;i<list.size();i++){
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+}
