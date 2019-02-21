ec227b26fba1f00792cd124e796810d883458914
caozheng <849138487@qq.com>
2019-02-21 22:18:31 +0800

    problem278 AC 第一个错误的版本

---
+++ src/leetcode/problem278/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.problem278;
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+//public class Solution extends VersionControl {
+//    public int firstBadVersion(int n) {
+//        int index = erfenSearch(n,1,n);
+//        return index;
+//    }
+//    public int erfenSearch(int n,int left,int right){
+//        if(left<=right){
+//            int mid = left+(right-left)/2;
+//            if(isBadVersion(mid)){
+//                return erfenSearch(n,left,mid-1);
+//            }else if(!isBadVersion(mid)){
+//                return erfenSearch(n,mid+1,right);
+//            }
+//        }
+//        if(left>right){
+//            return left;
+//        }
+//        return -1;
+//    }
+
+}
