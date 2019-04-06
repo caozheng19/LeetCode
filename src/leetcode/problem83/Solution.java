40b686def26f5fdf0b9f87f6d51a6b8ca7ef8d62
caozheng <849138487@qq.com>
2019-04-06 22:26:06 +0800

    problem83 AC 删除排序链表中的重复元素

--- src/leetcode/problem83/Solution.java
+++ src/leetcode/problem83/Solution.java
@@ -4,28 +4,43 @@ class ListNode {
       ListNode next;
       ListNode(int x) { val = x; }
 }
-
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if(head==null) return head;
-        ListNode preNode = head;
-        ListNode curNode = head.next;
-        while(preNode!=null){
-            if(curNode!=null){
-                //进行比较
-                if(preNode.val == curNode.val){
-                    preNode.next = curNode.next;
-                    curNode.next = null;
-                    curNode = preNode.next;
-                }else{
-                    preNode = preNode.next;
-                    curNode = curNode.next;
-                }
+        if(head==null||head.next==null){
+            return head;
+        }
+        ListNode cur = head;
+        while(cur!=null&&cur.next!=null){
+            if(cur.val==cur.next.val){
+                cur.next = cur.next.next;
             }else{
-                //从preNode节点算起只有一个节点 没必要进行操作 直接返回head
-                return head;
+                cur = cur.next;
             }
         }
         return head;
     }
 }
+//class Solution {
+//    public ListNode deleteDuplicates(ListNode head) {
+//        if(head==null) return head;
+//        ListNode preNode = head;
+//        ListNode curNode = head.next;
+//        while(preNode!=null){
+//            if(curNode!=null){
+//                //进行比较
+//                if(preNode.val == curNode.val){
+//                    preNode.next = curNode.next;
+//                    curNode.next = null;
+//                    curNode = preNode.next;
+//                }else{
+//                    preNode = preNode.next;
+//                    curNode = curNode.next;
+//                }
+//            }else{
+//                //从preNode节点算起只有一个节点 没必要进行操作 直接返回head
+//                return head;
+//            }
+//        }
+//        return head;
+//    }
+//}
