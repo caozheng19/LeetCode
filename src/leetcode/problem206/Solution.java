f9874c0841151625cd57f2436d8cc6419af9b44c
caozheng <849138487@qq.com>
2019-04-06 22:13:13 +0800

    problem206 AC 反转链表

--- src/leetcode/problem206/Solution.java
+++ src/leetcode/problem206/Solution.java
@@ -6,33 +6,47 @@ class ListNode {
      ListNode next;
      ListNode(int x) { val = x; }
   }
-
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head==null) return head;
-        ListNode pre = head;
-        ListNode cen = head.next;
-        ListNode behind = null;
-        ListNode newHead = null;
-        int flag =1;
-        if(cen!=null){
-            behind = cen.next;
+        if(head==null||head.next==null) return head;
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode temp = null;
+        while(cur!=null){
+            temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
         }
-        if(cen==null){
-            return head;
-        }
-        while(cen!=null){
-            if(flag==1){
-                pre.next = null;
-                flag=0;
-            }
-            cen.next = pre;
-            pre = cen;
-            newHead = cen;
-            cen = behind;
-            if(behind!=null)
-                behind = behind.next;
-        }
-        return newHead;
+        return pre;
     }
 }
+//class Solution {
+//    public ListNode reverseList(ListNode head) {
+//        if(head==null) return head;
+//        ListNode pre = head;
+//        ListNode cen = head.next;
+//        ListNode behind = null;
+//        ListNode newHead = null;
+//        int flag =1;
+//        if(cen!=null){
+//            behind = cen.next;
+//        }
+//        if(cen==null){
+//            return head;
+//        }
+//        while(cen!=null){
+//            if(flag==1){
+//                pre.next = null;
+//                flag=0;
+//            }
+//            cen.next = pre;
+//            pre = cen;
+//            newHead = cen;
+//            cen = behind;
+//            if(behind!=null)
+//                behind = behind.next;
+//        }
+//        return newHead;
+//    }
+//}
